1746e6063e44049d2bb3fe68626d0b4ec434370b
Joe Kahn <joekahn19@gmail.com>
2022-04-21 13:35:11 -0700

    commit tree de5ef8957e14acc1fc99c2c833d0794a52c80fcd

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+   
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Charlie", "Samip", "Ali"]
+const specialEvent = "birthday"
+
+function writeCards(names,specialEvent) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${specialEvent} gift!`);
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${specialEvent} gift!`)
+    }
+    return messages;
+}
+
+writeCards(names, specialEvent)
+
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+   
+}
